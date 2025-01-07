413eef968f2e54475eb31f5224bff9b5ae0fead3
mask <mask@gmail.com>
2025-01-07 14:27:03 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div class="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+.app {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
